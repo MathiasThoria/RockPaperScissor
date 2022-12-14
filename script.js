885898e70d9f153409dfa54e8b20ec74e3ea203f
MathiasThoria <mathiassoriapiro@gmail.com>
2022-12-14 03:12:40 -0300

    Add needed functions. Working on bugs

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 
+
+//this function takes playerSelection and computerSelection strings
+//and returns a string with the result of the match
 function playRound(playerSelection, computerSelection) {
     switch (playerSelection){
         case "scissors":
-            if computerSelection == "paper" {
+            if (computerSelection == "paper") {
                 return "You win! Scissor beats Paper";
             } else if (computerSelection== "rock"){
                 return "You lose! Rock beats Scissors";
@@ -11,7 +17,7 @@ function playRound(playerSelection, computerSelection) {
             }
         break;
         case "paper":
-            if computerSelection == "scissors" {
+            if (computerSelection == "scissors") {
                 return "You lose! Scissor beats Paper";
             } else if (computerSelection == "rock"){
                 return "You win! Paper beats Rock";
@@ -20,7 +26,7 @@ function playRound(playerSelection, computerSelection) {
             }
         break;
         case "rock":
-            if computerSelection == "paper" {
+            if (computerSelection == "paper") {
                 return "You lose! Paper beats Rock";
             } else if (computerSelection== "scissor"){
                 return "You win! Rock beats Scissors";
@@ -30,7 +36,7 @@ function playRound(playerSelection, computerSelection) {
         break;
     }
 }
-
+//this function returns a random string between s,r and p
 function getComputerChoice(){
     let rand = Math.random();
     console.log(rand)
@@ -43,6 +49,22 @@ function getComputerChoice(){
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game(){
+    let cont = 0;
+    let whowin = "";
+    for (let i=0; i<5; i++){
+        whowin = playRound(prompt("Scissors, Paper or Rock?"),computerSelection());
+        if (whowin.includes("win")) {
+            cont++;
+        } else if(whowin.includes("lose")){
+            cont--;
+        } else{ /* if tie, repeat */
+            i--;
+        }
+    }
+    if (cont<3){
+        console.log("Loooooooooser");        
+    }else{
+        console.log("Ok, you win")
+    }
+}
