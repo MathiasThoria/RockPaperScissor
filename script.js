dc8639cb451b725f089038f44406f0846a23bcb8
MathiasThoria <mathiassoriapiro@gmail.com>
2022-12-19 19:31:09 -0300

    Tecnical problems with git

--- script.js
+++ script.js
@@ -1,59 +1,138 @@
-const SCISSOR = "&#9986"
+const SCISSOR = '&#9986';
+const ROCK = '&#9968';
+const PAPER = '&#128195';
 //this function takes playerSelection and computerSelection strings
 //and returns a string with the result of the match
 function playRound(playerSelection, computerSelection) {
+    
+    console.log("La computadora eligio: " + computerSelection);
+    console.log("Tu elegiste: " + playerSelection);
+    
+
     switch (playerSelection){
-        case "scissors":
+        case "scissor":
             if (computerSelection == "paper") {
-                return "You win! Scissor beats Paper";
-            } else if (computerSelection== "rock"){
-                return "You lose! Rock beats Scissors";
+                return "win";
+            } else if (computerSelection == "rock"){
+                return "lose";
             } else {
-                return "Tie! Try again ";
+                return "tie";
             }
         break;
         case "paper":
-            if (computerSelection == "scissors") {
-                return "You lose! Scissor beats Paper";
+            if (computerSelection == "scissor") {
+                return "lose";
             } else if (computerSelection == "rock"){
-                return "You win! Paper beats Rock";
+                return "win";
             } else {
-                return "Tie! Try again ";
+                return "tie";
             }
         break;
         case "rock":
-            if (computerSelection == "paper") {
-                return "You lose! Paper beats Rock";
+            if (computerSelection == "rock") {
+                return "lose";
             } else if (computerSelection== "scissor"){
-                return "You win! Rock beats Scissors";
+                return "win";
             } else {
-                return "Tie! Try again ";
+                return "tie";
             }
+        break;
+        default:
+            return "no entro en ninguno";
         break;
     }
 }
 //this function returns a random string between s,r and p
 function getComputerChoice(){
     let rand = Math.random();
-    console.log(rand);
+    let choice = "";
+    const textPcSelection = document.querySelector(".pcSelection");
+        
+    console.log(rand);   
     if (rand < 0.3) {
-        return "scissors";
+        choice = "scissor";
+        textPcSelection.innerHTML=''+ SCISSOR; /*stranger things happend with emojis*/
     } else if (rand < 0.6) {
-        return "rock";
+        choice = "rock";
+        textPcSelection.innerHTML=''+ ROCK;
     } else {
-        return "paper";
-    }
+        choice = "paper";
+        textPcSelection.innerHTML=''+ PAPER;
+    };
+    
+    return choice;
+}
+
+function changerScoreboard(winOrNot){
+    console.log(winOrNot);
+    let yourResult = document.querySelector(".yourResult");
+    let PcResult = document.querySelector(".pcResult");    
+    let roundResult = document.querySelector(".result");
+    if (winOrNot == "win")
+        yourResult.textContent=+yourResult.textContent+1;
+    else if (winOrNot=="lose")
+        PcResult.textContent=+PcResult.textContent+1;
+    roundResult.textContent= winOrNot;
+
+}
+
+function reset(){
+    let yourResult = document.querySelector(".yourResult");
+    let PcResult = document.querySelector(".pcResult");    
+    let roundResult = document.querySelector(".result");
+    let pcSelection = document.querySelector(".pcSelection");
+    yourResult.textContent=0;
+    PcResult.textContent=0;
+    roundResult.textContent="...";
+    pcSelection.textContent=".";
 }
 
 function game(){
-    let cont = 0;
+    
+    /*let cont = 0;
     let playerSelectionString = "";
-    let whowin = "";
-    //round of 5 games
+    let whowin = "";*/
+    let oneRoundResult = "";
+    let pcPoints = 0;
+    let youPoints = 0;
+    let computerChoice = "";
+
+    const buttons = document.querySelectorAll('.conteinerContentA>button');
+    //console.log(buttons);
+    const textResultPc = document.querySelector('.pcResult');
+    const textResultYou = document.querySelector('.yourResult');
+
+
+    buttons.forEach(button => {
+        button.addEventListener('click',(e) =>{
+            console.log(button.textContent);
+            //alert(playRound(button.className,getComputerChoice()));
+            
+            computerChoice = getComputerChoice();      
+            oneRoundResult = playRound(button.className, computerChoice);
+            changerScoreboard(oneRoundResult);
+
+            if (textResultPc.textContent == 5){
+                alert("You loooose. Sorry.");
+                reset();
+            } 
+            if (textResultYou.textContent == 5){
+                alert("You win. Great!");
+                reset();
+            }
+            
+        });
+    }
+
+
+    );
+    
+
+  /*
     for (let i=0; i<5; i++){
-        //variable needed to lower case later
+        
         playerSelectionString = prompt("Scissors, Paper or Rock?");
-        //varieble to use .includes method
+      
         whowin = playRound(playerSelectionString.toLocaleLowerCase(),getComputerChoice());
         console.log(whowin);
 
@@ -61,7 +140,7 @@ function game(){
             cont++;
         } else if(whowin.includes("lose")){
             cont--;
-        } else{ /* if tie, repeat */
+        } else{ 
             i--;
         }
     }
@@ -70,6 +149,8 @@ function game(){
     }else{
         console.log("Ok, you win");
     }
+    */
 }
 
-//game();
+
+game();
